2950199b2f6ab5a66d004b6e1c3cb2884933ef05
Yelk11 <neighbour.matthew@gmail.com>
2020-05-05 12:30:00 -0400

    entry

--- app/src/main/java/m/yelk11/potalogger/models/LogEntryType.java
+++ app/src/main/java/m/yelk11/potalogger/models/LogEntryType.java
@@ -1,6 +1,8 @@
 package m.yelk11.potalogger.models;
 
-public class LogEntryType {
+import m.yelk11.potalogger.ui.fragments.LogEntryFragment;
+
+public class LogEntryType extends LogEntryFragment {
 
     private String name;
 
@@ -155,9 +157,311 @@ public class LogEntryType {
         WEB,
     }
 
-    ;
-
-    LogEntryType(String name) {
+    public LogEntryType(String name) {
         this.name = name;
+    }
+
+
+
+    public void createView(ADIF_types type) {
+        switch (type) {
+            case ADDRESS:
+
+                break;
+            case ADDRESS_INTL:
+                break;
+            case AGE:
+                break;
+            case A_INDEX:
+                break;
+            case ANT_AZ:
+                break;
+            case ANT_EL:
+                break;
+            case ANT_PATH:
+                break;
+            case ARRL_SECT:
+                break;
+            case AWARD_SUBMITTED:
+                break;
+            case AWARD_GRANTED:
+                break;
+            case BAND:
+                break;
+            case BAND_RX:
+                break;
+            case CALL:
+                break;
+            case CHECK:
+                break;
+            case CLASS:
+                break;
+            case CLUBLOG_QSO_UPLOAD_DATE:
+                break;
+            case CLUBLOG_QSO_UPLOAD_STATUS:
+                break;
+            case CNTY:
+                break;
+            case COMMENT:
+                break;
+            case COMMENT_INTL:
+                break;
+            case CONT:
+                break;
+            case CONTACTED_OP:
+                break;
+            case CONTEST_ID:
+                break;
+            case COUNTRY:
+                break;
+            case COUNTRY_INTL:
+                break;
+            case CQZ:
+                break;
+            case CREDIT_SUBMITTED:
+                break;
+            case CREDIT_GRANTED:
+                break;
+            case DARC_DOK:
+                break;
+            case EQSL_QSLSDATE:
+                break;
+            case EQSL_QSL_RCVD:
+                break;
+            case EQSL_QSL_SENT:
+                break;
+            case FISTS:
+                break;
+            case FISTS_CC:
+                break;
+            case FORCE_INIT:
+                break;
+            case FREQ:
+                break;
+            case FREQ_RX:
+                break;
+            case GRIDSQUARE:
+                break;
+            case GUEST_OP:
+                break;
+            case HRDLOG_QSO_UPLOAD_DATE:
+                break;
+            case HRDLOG_QSO_UPLOAD_STATUS:
+                break;
+            case IOTA:
+                break;
+            case IOTA_ISLAND_ID:
+                break;
+            case ITUZ:
+                break;
+            case K_INDEX:
+                break;
+            case LAT:
+                break;
+            case LON:
+                break;
+            case LOTW_QSLRDATE:
+                break;
+            case LOTW_QSLSDATE:
+                break;
+            case LOTW_QSL_RCVD:
+                break;
+            case LOTW_QSL_SENT:
+                break;
+            case MAX_BURSTS:
+                break;
+            case MODE:
+                break;
+            case MS_SHOWER:
+                break;
+            case MY_ANTENNA:
+                break;
+            case MY_ANTENNA_INTL:
+                break;
+            case MY_CITY:
+                break;
+            case MY_CITY_INTL:
+                break;
+            case MY_CNTY:
+                break;
+            case MY_COUNTRY:
+                break;
+            case MY_COUNTRY_INTL:
+                break;
+            case MY_CQ_ZONE:
+                break;
+            case MY_DXCC:
+                break;
+            case MY_FISTS:
+                break;
+            case MY_GRIDSQUARE:
+                break;
+            case MY_IOTA:
+                break;
+            case MY_IOTA_ISLAND_ID:
+                break;
+            case MY_ITU_ZONE:
+                break;
+            case MY_LAT:
+                break;
+            case MY_LON:
+                break;
+            case MY_NAME:
+                break;
+            case MY_NAME_INTL:
+                break;
+            case MY_POSTAL_CODE:
+                break;
+            case MY_POSTAL_CODE_INTL:
+                break;
+            case MY_RIG:
+                break;
+            case MY_RIG_INTL:
+                break;
+            case MY_SIG:
+                break;
+            case MY_SIG_INTL:
+                break;
+            case MY_SIG_INFO:
+                break;
+            case MY_SIG_INFO_INTL:
+                break;
+            case MY_SOTA_REF:
+                break;
+            case MY_STATE:
+                break;
+            case MY_STREET:
+                break;
+            case MY_STREET_INTL:
+                break;
+            case MY_USACA_COUNTIES:
+                break;
+            case MY_VUCC_GRIDS:
+                break;
+            case NAME:
+                break;
+            case NAME_INTL:
+                break;
+            case NOTES:
+                break;
+            case NOTES_INTL:
+                break;
+            case NR_BURSTS:
+                break;
+            case NR_PINGS:
+                break;
+            case OPERATOR:
+                break;
+            case OWNER_CALLSIGN:
+                break;
+            case PFX:
+                break;
+            case PRECEDENCE:
+                break;
+            case PROP_MODE:
+                break;
+            case PUBLIC_KEY:
+                break;
+            case QRZCOM_QSO_UPLOAD_DATE:
+                break;
+            case QRZCOM_QSO_UPLOAD_STATUS:
+                break;
+            case QSLMSG:
+                break;
+            case QSLMSG_INTL:
+                break;
+            case QSLRDATE:
+                break;
+            case QSLSDATE:
+                break;
+            case QSL_RCVD:
+                break;
+            case QSL_RCVD_VIA:
+                break;
+            case QSL_SENT:
+                break;
+            case QSL_SENT_VIA:
+                break;
+            case QSL_VIA:
+                break;
+            case QSO_COMPLETE:
+                break;
+            case QSO_DATE:
+                break;
+            case QSO_DATE_OFF:
+                break;
+            case QSO_RANDOM:
+                break;
+            case QTH:
+                break;
+            case QTH_INTL:
+                break;
+            case REGION:
+                break;
+            case RIG:
+                break;
+            case RIG_INTL:
+                break;
+            case RST_RCVD:
+                break;
+            case RST_SENT:
+                break;
+            case RX_PWR:
+                break;
+            case SAT_MODE:
+                break;
+            case SAT_NAME:
+                break;
+            case SFI:
+                break;
+            case SIG:
+                break;
+            case SIG_INTL:
+                break;
+            case SIG_INFO:
+                break;
+            case SIG_INFO_INTL:
+                break;
+            case SILENT_KEY:
+                break;
+            case SKCC:
+                break;
+            case SOTA_REF:
+                break;
+            case SRX:
+                break;
+            case SRX_STRING:
+                break;
+            case STATE:
+                break;
+            case STATION_CALLSIGN:
+                break;
+            case STX:
+                break;
+            case STX_STRING:
+                break;
+            case SUBMODE:
+                break;
+            case SWL:
+                break;
+            case TEN_TEN:
+                break;
+            case TIME_OFF:
+                break;
+            case TIME_ON:
+                break;
+            case TX_PWR:
+                break;
+            case UKSMG:
+                break;
+            case USACA_COUNTIES:
+                break;
+            case VE_PROV:
+                break;
+            case VUCC_GRIDS:
+                break;
+            case WEB:
+                break;
+        }
     }
 }
--- app/src/main/java/m/yelk11/potalogger/ui/fragments/LogEntryFragment.java
+++ app/src/main/java/m/yelk11/potalogger/ui/fragments/LogEntryFragment.java
@@ -1,5 +1,6 @@
 package m.yelk11.potalogger.ui.fragments;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.SharedPreferences;
@@ -14,11 +15,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import m.yelk11.potalogger.R;
+import m.yelk11.potalogger.models.LogEntryType;
 import m.yelk11.potalogger.ui.viewmodel.LogbookVM;
 
 public class LogEntryFragment extends Fragment {
@@ -47,27 +52,94 @@ public class LogEntryFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(LogbookVM.class);
 
 
-        LinearLayout llMain = getView().findViewById(R.id.log_entry_linear_layout);
+        LinearLayout linearLayout = getView().findViewById(R.id.log_entry_linear_layout);
 
 
         SharedPreferences sharedPreferences =
                 PreferenceManager.getDefaultSharedPreferences(getContext());
 
-        Boolean showGridsquares =
-                sharedPreferences.getBoolean("show_gridsquare", false);
-        if (showGridsquares) {
-            TextView textView = new TextView(getContext());
-            EditText editText = new EditText(getContext());
-            editText
 
-            editText.setText("I am added dynamically to the view");
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT
-            );
-            editText.setLayoutParams(params);
-            llMain.addView(editText);
+        LinearLayout.LayoutParams singleParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        LinearLayout.LayoutParams doubleParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        doubleParam.setLayoutDirection(LinearLayout.LAYOUT_DIRECTION_LTR);
+
+
+        // Date and Time
+        Boolean dateAndTime =
+                sharedPreferences.getBoolean("show_date_time", false);
+        if (true) {
+            TextView title = new TextView(getContext());
+            EditText timeAndDate = new EditText(getContext());
+            title.setText("Time and Date");
+            timeAndDate.setText("Enter Date Here");
+
+
+
+
+            linearLayout.addView(title);
+            linearLayout.addView(timeAndDate);
         }
+        //frequency
+
+        // Callsigns
+
+        //power report
+        Boolean powerReport =
+                sharedPreferences.getBoolean("power_report", false);
+        if (true) {
+            TextView title = new TextView(getContext());
+            EditText powerTx = new EditText(getContext());
+            EditText PowerRx = new EditText(getContext());
+            title.setText("Time and Date");
+            powerTx.setText("Enter Date Here");
+            PowerRx.setText("Enter Date Here");
+
+            powerTx.setLayoutParams(doubleParam);
+            PowerRx.setLayoutParams(doubleParam);
+
+            linearLayout.addView(title);
+            linearLayout.addView(powerTx);
+            linearLayout.addView(PowerRx);
+        }
+        // signal report
+
+        //grid square
+
+        //comments
+
+
+
+
+
+
+
+
+
+
+
+
+//        Boolean showGridsquares =
+//                sharedPreferences.getBoolean("show_gridsquare", false);
+//        if (showGridsquares) {
+//            TextView textView = new TextView(getContext());
+//            EditText editText = new EditText(getContext());
+//
+//
+//            editText.setText("I am added dynamically to the view");
+//            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.MATCH_PARENT,
+//                    LinearLayout.LayoutParams.MATCH_PARENT
+//            );
+//            editText.setLayoutParams(params);
+//            linearLayout.addView(editText);
+//        }
+
 
 
 
