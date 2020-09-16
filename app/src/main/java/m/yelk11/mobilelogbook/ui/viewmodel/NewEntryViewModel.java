4b45b6106e8dd047cc1af717414eafcc9cb95c32
Yelk11 <neighbour.matthew@gmail.com>
2020-09-16 09:43:06 -0400

    added icon

--- app/src/main/java/m/yelk11/mobilelogbook/ui/viewmodel/NewEntryViewModel.java
+++ app/src/main/java/m/yelk11/mobilelogbook/ui/viewmodel/NewEntryViewModel.java
@@ -1,9 +1,15 @@
 package m.yelk11.mobilelogbook.ui.viewmodel;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import m.yelk11.mobilelogbook.dbc.entity.Entry;
 import m.yelk11.mobilelogbook.repository.EntryRepository;
@@ -20,7 +26,35 @@ public class NewEntryViewModel extends AndroidViewModel {
     public void insert(Entry entry) {
         entryRepository.insert(entry);
     }
+
     public void update(Entry entry) {
         entryRepository.update(entry);
     }
+
+    public String getCurrentDate() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return dateFormat.format(date);
+    }
+
+    public void getGridsquare(){
+        //TODO: return gridsquare
+    }
+
+    public void getDefaultMode(){
+        // TODO: return default mode from settings
+    }
+
+    public void setDefaultMode(){
+        // TODO: set default mode for book only
+    }
+
+    public String getMyCallsign(){
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getApplication().getBaseContext());
+        String callsign = sharedPreferences.getString("callsign", "");
+        return callsign;
+    }
+
 }
